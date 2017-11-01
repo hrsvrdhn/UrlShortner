5f8a62eac8390a5341963e641be2496e33dc0340
Harsh Vardhan Sharma <harshavardhana.619@gmail.com>
2017-11-01 19:50:22 +0530

    api bug fix

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,6 @@ const homepage = require('./homepage');
 module.exports = function(app) {
 	app.get('/', homepage.get);
 	app.post('/', homepage.post);
-	app.get('/:shortCode', homepage.urlredirect);
 	app.post('/api', homepage.apipost);
+	app.get('/:shortCode', homepage.urlredirect);
 };
