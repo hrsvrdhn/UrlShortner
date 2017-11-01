3c036a4557189ef1b772ee33fdc50cfc7680c67d
Harsh Vardhan Sharma <harshavardhana.619@gmail.com>
2017-11-01 22:12:42 +0530

    Added headers

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
 var routes = require('./routes/index');
-var cors = require('cors');
 var port = process.env.PORT || 3000;
 if(process.env.NODE_ENV !== 'production')
 	var config = require('./config');
 var app = express();
-app.use(cors()) // for interacting with my chrome extension api
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DATABASE_URL || config.development.database, { useMongoClient : true });
 
